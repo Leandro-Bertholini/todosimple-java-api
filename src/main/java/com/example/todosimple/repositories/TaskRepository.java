aa031fbf4f8868d6db56fe6f8b3b512f9ed70386
Leandro Bertholini <bertholinileandro1@gmail.com>
2023-12-12 21:34:15 -0300

    add repositories/TaskRepository

---
+++ src/main/java/com/example/todosimple/repositories/TaskRepository.java
@@ -0,0 +1,14 @@
+package com.example.todosimple.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.todosimple.models.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+  List<Task> findByUser_id(long id);
+  
+}
